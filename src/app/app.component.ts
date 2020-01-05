465c1168fc72e35641de76cb45a475bbd20c59e1
Michael Krøyserth-Simsø <mikke.korgen@gmail.com>
2020-01-05 23:30:28 +0100

    feat: add google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
 
+declare let ga: Function;
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'michaelsimsoe';
+  title = "michaelsimsoe";
+
+  constructor(public router: Router) {
+    // subscribe to router events and send page views to Google Analytics
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        ga("set", "page", event.urlAfterRedirects);
+        ga("send", "pageview");
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { NavigationComponent } from './shared/navigation/navigation.component';
-import { HomeComponent } from './home/home.component';
-import { ArticleLinkComponent } from './home/article-link/article-link.component';
-import { SkillCircleComponent } from './home/skill-circle/skill-circle.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./shared/header/header.component";
+import { NavigationComponent } from "./shared/navigation/navigation.component";
+import { HomeComponent } from "./home/home.component";
+import { ArticleLinkComponent } from "./home/article-link/article-link.component";
+import { SkillCircleComponent } from "./home/skill-circle/skill-circle.component";
+import { ArticlesComponent } from "./articles/articles.component";
+import { Routes } from "@angular/router";
 
 @NgModule({
   declarations: [
@@ -16,13 +18,11 @@ import { SkillCircleComponent } from './home/skill-circle/skill-circle.component
     NavigationComponent,
     HomeComponent,
     ArticleLinkComponent,
-    SkillCircleComponent
+    SkillCircleComponent,
+    ArticlesComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
